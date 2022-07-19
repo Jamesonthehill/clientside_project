2ed8309ad66cc5b49403dc28b683ef8e62c95307
geotwo <dlrjsdn53@naver.com>
2022-07-19 09:07:04 +0900

    종화님과제 Mapper -> Service -> ServiceImpl -> Controller  resultType사용

---
+++ test/src/main/java/studyplace/study.java
@@ -0,0 +1,18 @@
+package studyplace;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class study {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<HashMap<String, Object>> alpha = new ArrayList<HashMap<String, Object>>();
+		
+		[] = new Array<Object>();
+		{} = new Map<String, Object>();
+		[{}, {}, {}] = new Array<Map<String, Object>>();
+
+	}
+
+}
