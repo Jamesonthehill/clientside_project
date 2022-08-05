2d8c64d1aa9f24425cd330b019a8de1c3d9f0081
geotwo <dlrjsdn53@naver.com>
2022-08-05 17:54:42 +0900

    2022-08-05 (금) 업로드

--- test/src/main/resources/static/js/layers.js
+++ test/src/main/resources/static/js/layers.js
@@ -4,6 +4,8 @@
 	
 	const init = () => {
 		
+		initEvent();
+		
 		//사용자 레이어 DB 물러옴
 		$.ajax({
 			async : false,
@@ -21,79 +23,71 @@
 				response.RESULT.map((value, index) => {
 					
 					let tr = document.createElement('tr');
-					tr.innerHTML += `<td>`;
-					tr.innerHTML += `	<input type="checkbox" id="checkbox${index}" style="zoom:2.0;margin-top:10px;">`;
-					tr.innerHTML += `	<label for="checkbox${index}" style="padding-left:20px;"><font size="4">${value.DATA_KOR_NM}</font></label>`;
+					
+					tr.id= index;
+					
+					tr.innerHTML += `<td style="border-radius:30px;">`;
+					tr.innerHTML += `	<input type="checkbox" id="checkbox${index}" style="zoom:2.0;margin-top:5px;">`;
+					tr.innerHTML += `	<font size="5" style="padding-left:15px;border-radius:15px;padding-right:15px;">${value.LYR_NM}</font>`;
 					tr.innerHTML += `</td>`;
 					
 					document.querySelector('#layer-list tbody').append(tr);
 					
 					tr.querySelector('input[type="checkbox"]').addEventListener('change', function() {
+						map.getO2LayerById(value.TABLE_NM).setVisible(this.checked);
+					});
+					
+					tr.querySelector('font').addEventListener('click', () => {
 						
-						console.log(value);
+						document.querySelectorAll(`#layer-list tr:not([id="${index}"])`).forEach(el => {
+							el.classList.remove('active');
+						});
 						
-						if (this.checked) {
-							console.log('레이어 on');
-							
+						if (tr.classList.contains('active')) {
+							tr.classList.remove('active');
 						} else {
-							console.log('레이어 off');
+							tr.classList.add('active');
 						}
 						
 					});
+						
+					tr.querySelector('font').addEventListener('mousedown', () => {
+						
+						console.log("우클릭이벤트 가즈아")
 					
-					// layer 불러오는 방법 s
-					var layer = new o2.layer.O2Layer({
+					});	
+					// layer 불러오는 방법
+					const layer = new o2.layer.O2Layer({
 					
 						source: new ol.source.TileWMS(({
 							url: o2.common.Config.getHostWMS(),
 							
 							params: {
-								'LAYERS': 'DBMS:SOL_GWLEE:' + value.DATA_ENG_NM,
+								'LAYERS': 'DBMS:SOL_GWLEE:' + value.TABLE_NM,
 								'TILED': true
 							}
 						})),
-						id: value.DATA_ENG_NM,
-						name: value.DATA_ENG_NM,
-						visible : value.VISIBLE_YN,		
+						id: value.TABLE_NM,
+						name: value.LYR_NM,
+						visible : false,		
 						opacity : value.OPACITY,
 						zIndex : value.SORT_ORDER
 					});
 				
 					// 'SVC' 레이어에 서울시 4종 레이어 추가
-					if(value.VISIBLE_YN == "Y"){
 					map.getO2GroupLayerById('SVC').getLayers().push(layer);
-					console.log(layer);
-					} else {
-						alert("레이어 작동 오류")
-					}
-					// layer 불러오는 방법 e 
 					
-					// layer on/Off s					
-					var layerChkBox = document.querySelectorAll('input[type=checkbox][name=layers]');
-	
-					for(var checkbox of layerChkBox) {
-						checkbox.addEventListener('change', function(e) {
-							var checkedLayer = map.getO2LayerById(e.target.id);
-							var checkedLayerId = checkedLayer.values_.id;
-							
-				            if(e.target.checked) {
-								if(e.target.id == checkedLayer.values_.id) {
-									checkedLayer.setVisible(true);
-									url = "/"+o2.common.Config.HOST.VALUES.APP_CONTEXT_PATH+"/updateLayerVisible.do"
-									param = {checkedLayerId: checkedLayerId, checkedLayerVisible: 'Y'};
-									updateLayer(url, param);
-								}
-							} else {
-								if(e.target.id == checkedLayer.values_.id) {
-									checkedLayer.setVisible(false);
-									param = {checkedLayerId: checkedLayerId, checkedLayerVisible: 'N'};
-									url = "/"+o2.common.Config.HOST.VALUES.APP_CONTEXT_PATH+"/updateLayerVisible.do";
-									updateLayer(url, param);
-								}
-							}
-						});
-					}
- 					// layer On/OFF e
+//					document.querySelectorAll("font").forEach(function(el){
+//						el.onclick = function() {
+//							console.log(this.innerText);
+//
+//							document.querySelectorAll("font").forEach(function(deselectEl) {
+//								deselectEl.style.backgroundColor='white';
+//							});
+//							
+//							this.style.backgroundColor='#e0f3f9';
+//						}
+//					});
 					
 				});
 				
@@ -107,40 +101,100 @@
 		
 	}
 	
-	
+	const initEvent = () => {
+		
+		// layer index change
+		document.querySelectorAll(".move-button").forEach(function(el) {
+			
+			el.addEventListener('click', () => {
+				
+				const activeTr = document.querySelector('#layer-list tr.active');
+				
+				if (activeTr === null) {
+					alert('활성화된 레이어가 없습니다.');
+					return;
+				}
+
+				if (el.id === 'up') {
+					
+					const previousTr = activeTr.previousSibling;
+					
+					if (previousTr === null) {
+						alert('이전 레이어가 없습니다.');
+						return;
+					}
+					
+					previousTr.before(activeTr);
+					
+				} else {
+					
+					const nextTr = activeTr.nextSibling;
+					
+					if (nextTr === null) {
+						alert('다음 레이어가 없습니다.');
+						return;
+					}
+					
+					nextTr.after(activeTr);
+					
+				}
+				
+			});
+			
+		});
+		
+	}
 	
 
 	
 	init(); // 이 init() 이 있어야지 위에서 써잇는 ajax 함수 영역위에 타고 올라가서 실행이된다.
 	
+	
+	
+	
 	console.log("init(); 다음 여기서 시작");
 	
-	// 오른쪽 마우스 이벤트
-	document.addEventListener('mousedown', function() {
+
 		
-		// Window에서 제공하는 마우스 우클릭 제거
+	// 오른쪽 마우스 이벤트
+//		const rightClick = document.getElementById('mouseup');
+//			
+//		rightClick.addEventListener('mousedown', function(e) {
+//					
+//			var isRightButton ;
+//			    
+//		    e = e || window.event;
+//			
+//		    if ("which" in e) {  // Gecko (Firefox), WebKit (Safari/Chrome) & Opera
+//		        isRightButton = e.which == 3; 
+//		    } else if ("button" in e) {  // IE, Opera 
+//		        isRightButton = e.button == 2; 
+//		
+//			}
+//	   		console.log("오른쪽 마우스 클릭");
+//		});
+			
+			
+//		const rightClick= (event.which == 3) || (event.button == 2);
+		
+//		window.oncontextmenu = function () {
+		    
+//			console.log(rightClick);
+
+//		     return rightClick;
+		
+			// Window에서 제공하는 마우스 우클릭 제거
+			
+//		}};
+//		element.addEventListener("mouseup", mouse_end);	
+
+		
 		window.oncontextmenu = function () {
 			
-			console.log("마우스 우클릭 안됨");
+			console.log("window 마우스 우클릭 안됨");
 		  
 		  	return false;
 		};
 		
-//			document.getElementById("button").mousedown = function() {
-//				
-//	    		("#boxWrap").append("<button class='original'></button>");
-//			    	
-//	    		console.log("동작확인");
-//		    	
-//	    		my_btn.addEventListener('click', () => {
-//						  
-//			 		alert('안녕하세요!!');
-//						
-//				}, { once : true});
-//			    
-//
-//			};
-		
-	});
 		
 })();
